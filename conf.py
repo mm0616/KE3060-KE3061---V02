c6e02261203d88df87aac6f8de0d7c5488366f53
momo <1668817352@qq.com>
2025-03-18 14:58:30 +0800

    first commit

--- conf.py
+++ conf.py
@@ -27,7 +27,7 @@ html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
 
 html_theme_options = {
-    "repository_url": "https://github.com/{546456456456}",
+    "repository_url": "https://github.com/546456456456",
     "use_repository_button": True,
 }
 
